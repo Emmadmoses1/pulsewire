5ac67b5289e8d1d5f66e3063d9d5e50298af31f8
LTC Mining <ltcmining@website.com>
2026-09-18 10:29:45 +0100

    Serve /images folder for favicon and logos

--- server-app.js
+++ server-app.js
@@ -91,6 +91,7 @@ app.use('/posts', express.static(path.join(__dirname, 'posts'), { extensions: ['
 app.use('/songs', express.static(path.join(__dirname, 'songs'), { extensions: ['html'] }));
 app.use('/artists', express.static(path.join(__dirname, 'artists'), { extensions: ['html'] }));
 app.use('/uploads', express.static(UPLOADS_DIR));
+app.use('/images', express.static(path.join(__dirname, 'images')));
 app.get('/admin.html', (req, res) => res.sendFile(path.join(__dirname, 'admin.html')));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
